7196425d328f0c268bd93ab1251a27e8fe6a56ec
dylan <090445@edu.nhtv.nl>
2013-03-12 14:41:53 +0100

    popup update

--- js/basic.js
+++ js/basic.js
@@ -13,8 +13,45 @@ jQuery(function ($) {
 	//$('#basic-modal-content').modal();
 
 	// Load dialog on click
-	$('#basic-modal .basic').click(function (e) {
-		$('#basic-modal-content').modal();
+	$('#basic-modal .prod1').click(function (e) {
+		$('#prod1').modal();
+
+		return false;
+	});
+	
+	$('#basic-modal .prod2').click(function (e) {
+		$('#prod2').modal();
+
+		return false;
+	});
+	
+	$('#basic-modal .prod3').click(function (e) {
+		$('#prod3').modal();
+
+		return false;
+	});
+	
+	$('#basic-modal .tact1').click(function (e) {
+		$('#tact1').modal();
+
+		return false;
+	});
+	
+	$('#basic-modal .tact2').click(function (e) {
+		$('#tact2').modal();
+
+		return false;
+	});
+	
+	$('#basic-modal .tact3').click(function (e) {
+		$('#tact3').modal();
+
+		return false;
+	});
+	
+	
+	$('#basic-modal .basic1').click(function (e) {
+		$('#basic1-modal-content').modal();
 
 		return false;
 	});
